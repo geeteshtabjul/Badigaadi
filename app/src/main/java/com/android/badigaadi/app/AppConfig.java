6a4c0a47b5d790da07877e9de96c92600b0599b5
Aman Dalmia <amandalmia18@gmail.com>
2015-11-01 19:15:20 +0530

    Login Register Committed

---
+++ app/src/main/java/com/android/badigaadi/app/AppConfig.java
@@ -0,0 +1,8 @@
+package com.android.badigaadi.app;
+
+public class AppConfig {
+	public static String URL_LOGIN = "http://192.168.117.1/android_login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://192.168.117.1/android_login_api/register.php";
+}
